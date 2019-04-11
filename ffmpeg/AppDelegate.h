0f1d180f2f23ef1b798a5766a941dca4581e14b6
zhangxin01 <zhangxin01@ainirobot.com>
2019-04-11 21:57:34 +0800

    Initial Commit

---
+++ ffmpeg/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ffmpeg
+//
+//  Created by zhangxin on 2019/4/11.
+//  Copyright © 2019年 zhangxin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
